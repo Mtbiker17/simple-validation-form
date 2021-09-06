fa19b24ff473593f04577a7430663328e46d96d3
Devin <ospreywebservices@gmail.com>
2021-09-06 10:55:48 -0400

    adding js for live validation, name and email done

---
+++ script.js
@@ -0,0 +1,35 @@
+const nameField = document.getElementById('name');
+const email = document.getElementById('mail');
+const submit = document.getElementById('submit');
+const input = document.querySelectorAll('input');
+
+nameField.addEventListener('focusout', (e) => {
+    if(nameField.value == null || nameField.value == '') {
+        nameField.setCustomValidity('Name cannot be blank')
+    } else {
+        nameField.setCustomValidity('');
+    }
+});
+
+email.addEventListener('input', (e) => {
+    if(email.validity.typeMismatch) {
+        email.setCustomValidity('Please Enter a Valid Email Address');
+    } else {
+        email.setCustomValidity('');
+    }
+});
+
+//this will be later on after completing validity checks
+//may use HTMLInputElement??
+/*submit.addEventListener('click', (e) => {
+    input.forEach(input => {
+        if(input.value === ''){
+            e.preventDefault();
+            console.log('no')
+        }
+        if(input.checkValidity() === false){
+            e.preventDefault();
+            console.log('not valid')
+        }
+    })
+})*/
