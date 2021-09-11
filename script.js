7d582e4919f82ee88c3185f5455b98564de31105
Devin <ospreywebservices@gmail.com>
2021-09-11 12:46:30 -0400

    removed .com requirement for email, added '.' instead to require in email address input. Added required attribute to all form elements

--- script.js
+++ script.js
@@ -2,61 +2,66 @@ const nameField = document.getElementById('name');
 const email = document.getElementById('mail');
 const submit = document.getElementById('submit');
 const input = document.querySelectorAll('input');
-const country = document.getElementById('country')
+const country = document.getElementById('country');
 const zipcode = document.getElementById('zipcode');
 const password = document.getElementById('password');
 const verify = document.getElementById('verify');
 
 function emailValidityCheck() {
-    if(email.validity.typeMismatch || !email.value.includes('.com') ) {
-        email.setCustomValidity('Please Enter a Valid Email Address');
-    } else {
-        email.setCustomValidity('');
-    }
+  if (email.validity.typeMismatch || !email.value.includes('.')) {
+    email.setCustomValidity('Please Enter a Valid Email Address');
+  } else {
+    email.setCustomValidity('');
+  }
 }
 
 function nameValidityCheck() {
-    if(nameField.value == null || nameField.value.trim() == '') {
-        nameField.setCustomValidity('Name cannot be blank')
-    } else {
-        nameField.setCustomValidity('');
-    }
+  if (nameField.value == null || nameField.value.trim() == '') {
+    nameField.setCustomValidity('Name cannot be blank');
+  } else {
+    nameField.setCustomValidity('');
+  }
 }
 
 function countryValidityCheck() {
-    if(country.value === ''){
-        country.setCustomValidity('Please select a country');
-    } else {
-        country.setCustomValidity('');
-    }
-};
+  if (country.value === '') {
+    country.setCustomValidity('Please select a country');
+  } else {
+    country.setCustomValidity('');
+  }
+}
 
 function zipValidityCheck() {
-    if(zipcode.validity.patternMismatch || zipcode.value === '') {
-        zipcode.setCustomValidity('Please Enter a 5 Digit Zip Code');
-    } else {
-        zipcode.setCustomValidity('');
-    }
-};
+  if (zipcode.validity.patternMismatch || zipcode.value === '') {
+    zipcode.setCustomValidity('Please Enter a 5 Digit Zip Code');
+  } else {
+    zipcode.setCustomValidity('');
+  }
+}
 
 function passwordValidityCheck() {
-    let passwordCheck = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{12,})");
-    if(password.value.match(passwordCheck)){
-        password.setCustomValidity('');
-    } else {
-        password.setCustomValidity('Password must be 12 characters long, include 1 lowercase, 1 uppercase, 1 numeric character and one special character')
-    }
+  let passwordCheck = new RegExp(
+    '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{12,})'
+  );
+  if (password.value.match(passwordCheck)) {
+    password.setCustomValidity('');
+  } else {
+    password.setCustomValidity(
+      'Password must be 12 characters long, include 1 lowercase, 1 uppercase, 1 numeric character and one special character'
+    );
+  }
 }
 
 function verifyPassword() {
-    if(verify.value !== password.value){
-        verify.setCustomValidity('Passwords Do Not Match');
-    } else {
-        verify.setCustomValidity('');
-    }
+  if (verify.value !== password.value) {
+    verify.setCustomValidity('Passwords Do Not Match');
+  } else {
+    verify.setCustomValidity('');
+  }
 }
 
 verify.addEventListener('input', verifyPassword);
+verify.addEventListener('focusin', verifyPassword);
 
 password.addEventListener('input', passwordValidityCheck);
 password.addEventListener('focusin', passwordValidityCheck);
@@ -71,12 +76,11 @@ email.addEventListener('focusin', emailValidityCheck);
 email.addEventListener('focusout', emailValidityCheck);
 
 nameField.addEventListener('input', nameValidityCheck);
-nameField.addEventListener('focusout', nameValidityCheck)
+nameField.addEventListener('focusout', nameValidityCheck);
 nameField.addEventListener('focusin', nameValidityCheck);
 
 country.addEventListener('change', countryValidityCheck);
 country.addEventListener('focusout', countryValidityCheck);
-
 
 //this will be later on after completing validity checks
 //may use HTMLInputElement??
